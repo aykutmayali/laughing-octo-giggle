55ce0badfa7411f60559b4c6f02919b164de59b5
aykutmayali <aykutmayali@gmail.com>
2020-09-06 19:57:40 +0300

    first commit

---
+++ stacks.js
@@ -0,0 +1,22 @@
+/* Stacks */
+/*push pop peek length*/
+
+var letters= [];
+var word = "oppo";
+var rword= "";
+
+for (var i = 0; i < word.length; i++) {
+  letters.push(word[i]);
+}
+
+console.log(letters);
+
+for (var i = 0; i <word.length;i++){
+  rword += letters.pop(); 
+}
+
+if(rword === word){
+  console.log(word + "is a palindrome");
+} else {
+  console.log(word + "is not a palindrome");
+}
