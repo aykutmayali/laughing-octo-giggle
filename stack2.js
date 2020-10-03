42a1a1a597588bb5ac27b92edd55c0a8f134bcb3
aykutmayali <aykutmayali@gmail.com>
2020-10-03 18:35:23 +0300

    stack 2 revised

--- stack2.js
+++ stack2.js
@@ -2,26 +2,28 @@ var Stack = function(){
     this.count = 0;
     this.storage = {};
 
+    // adds value onto the end of the stack
     this.push = function(value){
         this.storage[this.count]= value;
         this.count++;
     }
 
+    //removes and returns the value at the end of the stack
     this.pop = function(){
         if(this.count ===0){
             return undefined;
         }
+    
+        this.count--;
+        var result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
     }
-
-    this.count--;
-    var result = this.storage[this.count];
-    delete this.storage[this.count];
-    return result;
-
     this.size = function(){
         return this.count;
     }
 
+    //return the last element of the array without removing
     this.peek = function (){
         return this.storage[this.count-1];
     }
@@ -29,12 +31,42 @@ var Stack = function(){
 
 var myStack = new Stack();
 
-myStack.push(1);
-myStack.push(2);
+mystack.push(1);
+mystack.push(2);
 
-console.log(myStack.peek);
-console.log(myStack.pop);
-console.log(myStack.peek);
+console.log(mystack.peek);
+
+
+console.log(mystack.pop);
+
+
+console.log(mystack.peek);
+
+
+mystack.push(3);
+
+console.log(mystack.peek);
+
+mystack.push("dede");
+
+console.log(mystack.peek);
+
+console.log(mystack.size);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
