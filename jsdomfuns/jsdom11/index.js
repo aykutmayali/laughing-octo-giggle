6edaa5f4377c4158440dead33ea9078c33cd2d42
aykutmayali <aykutmayali@gmail.com>
2021-01-31 00:08:01 +0300

    emojis

---
+++ jsdomfuns/jsdom11/index.js
@@ -0,0 +1,37 @@
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+const emojiContainer = document.getElementById("emojiContainer")
+
+// console.log(emojiContainer)
+
+
+
+// for(let i=0; i<myEmojis.length ; i++){
+//     console.log(myEmojis[i]);
+//     const emoji = document.createElement('span')
+//     emoji.textContent = myEmojis[i]
+//     emojiContainer.append(emoji)
+// }
+
+// myEmojis.forEach(element => console.log(element));
+
+function renderEmojis() {
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+
+renderEmojis()
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        emojiContainer.innerHTML = ""
+        renderEmojis()
+    }
+})
