48c5b7767db4971fe0855de21fbb2dd3dbd583f0
aykutmayali-osf <85240025+aykutmayali-osf@users.noreply.github.com>
2021-06-07 09:18:27 +0300

    prtotypes

--- cleancode/1/index.js
+++ cleancode/1/index.js
@@ -7,3 +7,46 @@ function getUsersFullName(userNames){
 
 console.log(getUsersFullName(['İbrahim', 'Asaf', 'Yalcin']));
 
+class Cat {
+    name;
+    age;
+}
+
+const tito = new Cat();
+tito.age = 12;
+tito.name = 'tito';
+
+function Dalmatian (name) {
+    this.name = name;
+  }
+  
+  Dalmatian.prototype.bark = function() {
+    console.log(`${this.name} barks!`);
+  };
+
+  // function Dog(age, weight, name) {
+//   this.age = age;
+//   this.weight = weight;
+//   this.name = name;
+//   this.bark = function () {
+//     console.log(`${this.name} says woof!`);
+//   };
+// }
+
+function Dog(age, weight, name) {
+    this.age = age;
+    this.weight = weight;
+    this.name = name;
+  }
+  
+  Dog.prototype.bark = function () {
+      console.log(`${this.name} says woof!`);
+  };
+  
+  dog1 = new Dog(2, 60, 'Java');
+  
+  dog2 = new Dog(4, 55, 'Jodi');
+  
+  dog1.bark();
+  
+  dog2.bark();
